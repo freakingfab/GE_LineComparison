51a6284a48cae136bfe871513146e82c07ecc0a6
Daksh <dakshnahar2001@gmail.com>
2023-11-26 17:47:35 +0530

    Added: compareTo function

--- LineComparison.java
+++ LineComparison.java
@@ -18,36 +18,55 @@ public class LineComparison {
         return Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2));
     }
 
+//    @desc: it provides comparison between two lines
+//    @params: LineComparison l1,l2
+//    @return: int
+    public int compareTo(LineComparison line2){
+        double len1= this.lengthLine();
+        double len2= line2.lengthLine();
+        if(len1==len2){
+            return 0;
+        }
+        else if(len1>len2){
+            return 1;
+        }
+        else{
+            return 2;
+        }
+    }
+
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.println("Input for Line 1");
-        System.out.print("Enter x1, y1");
+        System.out.print("Enter x1, y1: ");
         int x1l1 = input.nextInt(), y1l1= input.nextInt();
 
         System.out.println("Input for Line 1");
-        System.out.print("Enter x2, y2");
+        System.out.print("Enter x2, y2: ");
         int x2l1 = input.nextInt(), y2l1= input.nextInt();
 
         LineComparison line1= new LineComparison(x1l1,y1l1,x2l1, y2l1);
 
         System.out.println("Input for Line 2");
-        System.out.print("Enter x1, y1");
+        System.out.print("Enter x1, y1: ");
         int x1l2 = input.nextInt(), y1l2= input.nextInt();
 
         System.out.println("Input for Line 2");
-        System.out.print("Enter x2, y2");
+        System.out.print("Enter x2, y2: ");
         int x2l2 = input.nextInt(), y2l2= input.nextInt();
 
         LineComparison line2= new LineComparison(x1l2,y1l2,x2l2, y2l2);
 
-        double len1= line1.lengthLine();
-        double len2= line2.lengthLine();
+        int val= line1.compareTo(line2);
 
-        if(len1==len2){
+        if(val==0){
             System.out.println("Both Lines are equal");
         }
+        else if(val==1){
+            System.out.println("Line 1 is greater");
+        }
         else{
-            System.out.println("Lines are not equal");
+            System.out.print("Line 2 is greater");
         }
     }
 }
